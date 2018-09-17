c6165a8a543c4762b8ab1681f2bcfaa37bf67681
riyazzzmohaideen <43335355+riyazzzmohaideen@users.noreply.github.com>
2018-09-17 14:25:36 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  int str[7];
+  if(4==101)
+    printf("the binary representation of the number is :");
+  else
+    printf("error");
+}
+  
